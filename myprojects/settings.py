d86d308c99657eb4d392c5a534fe42f8003aa835
Sakir02 <154803569+Sakir02@users.noreply.github.com>
2026-06-11 11:02:55 +0530

    aa
    a

--- myprojects/settings.py
+++ myprojects/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,10 +64,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myprojects.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR/'db.sqlite3'}"
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
